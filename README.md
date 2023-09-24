7af6e2b15badd0ad9cfc9c9e1e52aeae87e85dd5
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-09-24 17:43:58 +0200

    Starter template for React added

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# react-vite-starter
- 
+# RACE Your React 🎉
+
+Get started with React in Vite quickly using this starter template. It includes HMR (Hot Module Replacement) and essential ESLint rules.
+
+A RACE customisation of the original Vite Starter Template available at https://vitejs.dev/guide.
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import reactLogo from "./assets/react.svg";
+
+function App() {
+    return (
+        <>
+            <header>
+                <a href="https://react.dev" target="_blank" rel="noreferrer">
+                    <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+                <h1>RACE Your React 🎉</h1>
+            </header>
+        </>
+    );
+}
+
+export default App;
